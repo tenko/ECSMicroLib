3eb648ed13e9729579a4de629b822ca0be25de35
Runar Tenfjord <runar.tenfjord@gmail.com>
2026-07-27 12:54:17 +0200

    Linux support STM32C5

--- boards/STM32C5-EVAL/build.mk
+++ boards/STM32C5-EVAL/build.mk
@@ -1,8 +1,16 @@
 # Board specific targets
-MXPROGDIR = ${USERPROFILE}/AppData/Local/stm32cube/bundles/programmer/2.22.0+st.1/bin
-MXPROG = ${MXPROGDIR}/STM32_Programmer_CLI.exe
-MXSTLINK = ${USERPROFILE}/AppData/Local/stm32cube/bundles/stlink-gdbserver/7.13.0+st.3/bin/ST-LINK_gdbserver.exe
-MXGDB = /c/Users/rute/AppData/Local/stm32cube/bundles/gnu-gdb-for-stm32/14.3.1+st.2/bin/arm-none-eabi-gdb.exe
+
+ifdef MSYSTEM
+	MXPROGDIR = ${USERPROFILE}/AppData/Local/stm32cube/bundles/programmer/2.22.0+st.1/bin
+	MXPROG = ${MXPROGDIR}/STM32_Programmer_CLI.exe
+	MXSTLINK = ${USERPROFILE}/AppData/Local/stm32cube/bundles/stlink-gdbserver/7.13.0+st.3/bin/ST-LINK_gdbserver.exe
+	MXGDB = /c/Users/rute/AppData/Local/stm32cube/bundles/gnu-gdb-for-stm32/14.3.1+st.2/bin/arm-none-eabi-gdb.exe
+else
+	MXPROGDIR = /opt/stm32cubeprog/bin/
+	MXPROG = ${MXPROGDIR}/STM32_Programmer_CLI
+	MXSTLINK = /opt/stm32cubeide/plugins/com.st.stm32cube.ide.mcu.externaltools.stlink-gdb-server.linux64_2.2.500.202604010938/tools/bin/ST-LINK_gdbserver
+	MXGDB = arm-none-eabi-gdb
+endif
 
 .PHONY: mxflash
 mxflash: build/test.rom
